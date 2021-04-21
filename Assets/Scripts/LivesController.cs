7e65101d16088157ee9fb6f10ca281e21c58b424
Shayla Hinkley <hinkleys@mail.gvsu.edu>
2021-04-21 12:20:59 +0000

    Add lives text

---
+++ Assets/Scripts/LivesController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LivesController : MonoBehaviour
+{
+    public Text lives;
+    public static int lifeCount = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        lives = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives.text = "Score: " + lifeCount;
+    }
+}
